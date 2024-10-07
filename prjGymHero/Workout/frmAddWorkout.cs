4e81d4ccf83107a0d1db5fea03238ddf4731ac50
Daan Hautekeete <hautekeete.daan@gmail.com>
2024-10-07 12:47:04 +0200

    File Structure
    Project creation + File structure
    
    Co-Authored-By: Luka Verbrugghe <135641479+LukaVerbrugghe@users.noreply.github.com>

---
+++ prjGymHero/Workout/frmAddWorkout.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prjGymHero.Workout
+{
+    public partial class frmAddWorkout : Form
+    {
+        public frmAddWorkout()
+        {
+            InitializeComponent();
+        }
+    }
+}
